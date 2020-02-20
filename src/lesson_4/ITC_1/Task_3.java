458015c5943e51256a02a6926faa6360868ddbf1
Egor Molkov <molkov@i-teco.ru>
2020-02-20 20:32:00 +0300

    new commit

--- src/lesson_4/ITC_1/Task_3.java
+++ src/lesson_4/ITC_1/Task_3.java
@@ -19,14 +19,15 @@ public class Task_3 {
     }
 
     public static double percent(double a, double b) {
-        return a * b/100;
+        return a * b / 100;
     }
 
 
     public static void main(String[] args) {
-        System.out.println(subtraction(2,23.2));
-        System.out.println(division(1,3));
-        System.out.println(multiplication(0.33, 0.33));
-        System.out.println(percent(23.23,12.1));
+        System.out.println("Вычитание: " + subtraction(2, 23.2));
+        System.out.println("Сложение: " + addition(2, 23.2));
+        System.out.println("Деление: " + division(1, 3));
+        System.out.println("Умножение: " + multiplication(0.33, 0.33));
+        System.out.println("Процент: " + percent(23.23, 12.1));
     }
 }
